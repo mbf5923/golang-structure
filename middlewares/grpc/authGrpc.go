e21d3cb3c960667a1b398e0adb696031522e4443
mbf5923 <mbf5923@yahoo.com>
2023-06-02 15:52:00 +0330

    set in response header check token reference

--- middlewares/grpc/authGrpc.go
+++ middlewares/grpc/authGrpc.go
@@ -9,7 +9,6 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/metadata"
-	"log"
 	modelUser "mbf5923.com/todo/domain/user/models"
 	authPb "mbf5923.com/todo/servicepb/authpb"
 	util "mbf5923.com/todo/utils"
@@ -38,34 +37,38 @@ func Auth(redisConnection *redis.Client) gin.HandlerFunc {
 		defer func(grpcClient *grpc.ClientConn) {
 			err := grpcClient.Close()
 			if err != nil {
-				log.Printf("Error while calling sum RPC: %v", err)
+				util.APIResponse(ctx, "GRPC Server Error", http.StatusInternalServerError, ctx.Request.Method, nil)
+				defer ctx.AbortWithStatus(http.StatusInternalServerError)
+				return
 			}
 		}(grpcClient)
 		authServer := authPb.NewAuthServiceClient(grpcClient)
 		var user modelUser.EntityUsers
-		err = doAuth(authServer, token, &user, *redisConnection)
+		fromCache, err := doAuth(authServer, token, &user, *redisConnection)
 		if err != nil {
-			util.APIResponse(ctx, "AccessToken invalid or expired", http.StatusUnauthorized, ctx.Request.Method, nil)
-			defer ctx.AbortWithStatus(http.StatusUnauthorized)
+			util.APIResponse(ctx, "GRPC Server Error", http.StatusInternalServerError, ctx.Request.Method, nil)
+			defer ctx.AbortWithStatus(http.StatusInternalServerError)
 			return
 		} else {
 			// global value result
 			ctx.Set("user", &user)
+			//set header token checked from cache
+			ctx.Header("Token-From-Cache", fmt.Sprintf("%t", fromCache))
 			// return to next method if token is exists
 			ctx.Next()
 		}
 	})
 }
 
-func doAuth(authServer authPb.AuthServiceClient, token string, user *modelUser.EntityUsers, redisConnection redis.Client) error {
+func doAuth(authServer authPb.AuthServiceClient, token string, user *modelUser.EntityUsers, redisConnection redis.Client) (bool, error) {
 	ctx := context.Background()
 	userFromRedis, err := redisConnection.Get(ctx, token).Result()
 	if err == nil {
 		unmarshalErr := json.Unmarshal([]byte(userFromRedis), &user)
 		if unmarshalErr != nil {
-			return err
+			return true, err
 		}
-		return nil
+		return true, nil
 	}
 	req := &authPb.AuthRequest{
 		Token: token,
@@ -74,15 +77,13 @@ func doAuth(authServer authPb.AuthServiceClient, token string, user *modelUser.E
 	ctx = metadata.NewOutgoingContext(ctx, metadata.Pairs("serviceName", "TaskService"))
 	res, err := authServer.Auth(ctx, req)
 	if err != nil {
-		log.Printf("Error while calling sum RPC: %v", err)
-		return err
+		return false, err
 	}
 	js, err := json.Marshal(res)
 	if err != nil {
-		log.Printf("Error while calling sum RPC: %v", err)
-		return err
+		return false, err
 	}
 	err = json.Unmarshal(js, &user)
 
-	return nil
+	return false, nil
 }
